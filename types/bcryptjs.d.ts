7a4cc79a72907183f287c393c1780ce74dc86654
benaleo <benaleo.bayu.satria@unictive.id>
2025-09-08 15:07:35 +0700

    try fix err

--- types/bcryptjs.d.ts
+++ types/bcryptjs.d.ts
@@ -1,5 +1,8 @@
 declare module 'bcryptjs' {
-  const compare: (data: string, encrypted: string) => Promise<boolean>
-  const hash: (data: string, salt: number | string) => Promise<string>
-  export { compare, hash }
+  export interface Bcrypt {
+    compare(data: string, encrypted: string): Promise<boolean>
+    hash(data: string, salt: number | string): Promise<string>
+  }
+  const bcrypt: Bcrypt
+  export default bcrypt
 }
